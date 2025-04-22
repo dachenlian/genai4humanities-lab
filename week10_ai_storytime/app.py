25771128f61a414a071742be9bfe47651330dc13
dachenlian <cclemon.lian@gmail.com>
2025-04-23 00:20:11 +0800

    enhance chatbot functionality and improve text formatting in page display

--- week10_ai_storytime/app.py
+++ week10_ai_storytime/app.py
@@ -3,6 +3,8 @@ from pathlib import Path
 
 import gradio as gr
 
+from ai_storytime import prompts as p
+
 story_name = "story"
 DATA_DIR = Path(__file__).parent / "data"
 STORY_DIR = DATA_DIR / story_name
@@ -20,7 +22,9 @@ pages = story["pages"]
 selected_image = None
 selected_text = None
 
-with gr.Blocks(theme=gr.themes.Citrus()) as demo:
+with gr.Blocks(
+    theme=gr.themes.Citrus()  # type: ignore
+) as demo:
     # TODO: allow user upload of data (json with text and img paths; images in img folder)
     gr.Markdown(f"# {title if (title := story['title']) else 'Untitled'}")
     page_radio = gr.Radio(
@@ -28,12 +32,41 @@ with gr.Blocks(theme=gr.themes.Citrus()) as demo:
         choices=[(k.title(), k) for k in pages.keys()],
         interactive=True,
     )
+    with gr.Row():
+        chatbot = gr.Chatbot(type="messages", label="和我聊聊故事吧！")
+    with gr.Row():
+        msg = gr.Textbox()
+    with gr.Row():
+        clear = gr.ClearButton([msg, chatbot])
+
+        def respond(message: str, chat_history: list[gr.ChatMessage]):
+            print(selected_image, selected_text)
+            if not selected_image:
+                raise gr.Error(
+                    "請先選擇一頁以獲取圖片和文本！", duration=5)
+            if not chat_history and selected_image and selected_text:
+                initial_prompt = p.prepare_answer_illustration_question_prompt(
+                    img_path=selected_image,
+                    book_text=selected_text,
+                    user_question=message,
+                )
+                chat_history.append(gr.ChatMessage(role="user", content=message))
+                assistant_response = "Test"
+                chat_history.append(gr.ChatMessage(role="assistant", content=assistant_response))
+            else:
+                chat_history.append(gr.ChatMessage(role="user", content=message))
+                assistant_response = "Test"
+                chat_history.append(gr.ChatMessage(role="assistant", content=assistant_response))
+
+            return "", chat_history
+        
+        msg.submit(respond, [msg, chatbot], [msg, chatbot], queue=False)
+
+
+
     with gr.Row(equal_height=True):
-        page_text = gr.Textbox(
+        page_text = gr.Markdown(
             label="Text",
-            lines=20,
-            interactive=False,
-            placeholder="Select a page to see the text.",
         )
         page_img = gr.Image(
             label="Image",
@@ -42,12 +75,16 @@ with gr.Blocks(theme=gr.themes.Citrus()) as demo:
             show_label=True,
             height=500,
         )
+    
 
     @page_radio.change(inputs=page_radio, outputs=[page_text, page_img])
     def show_page_content(choice):
         global selected_text, selected_image
         page = pages[choice]
-        text = page["text"]
+        text = page['text'].split("\n")
+        text = [f"# {line}" for line in text]
+        text = "\n".join(text)
+
         img_name = page["img"]
         img_path = IMG_DIR / img_name
         selected_text = text
